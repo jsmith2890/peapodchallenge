fc03a8a012f8d166f2e2bb75ed5b21b89a1a39ca
Jessica Smith <smith.j.m.13@gmail.com>
2020-03-10 17:01:46 -0500

    rendered repo list for each user

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, TouchableOpacity} from 'react-native';
+import {ListItem} from 'react-native-elements';
 
 const RepoList = ({repos}) => (
-  <View>
-    <Text>hiiiii</Text>
-  </View>
+  <ScrollView>
+    <Text>Repos</Text>
+    {repos.map(repo => (
+      <TouchableOpacity onPress={() => console.log('hi')} key={repo.id}>
+        <ListItem
+          key={repo.id}
+          title={repo.name}
+          subtitle={repo.description}
+          bottomDivider
+        />
+      </TouchableOpacity>
+    ))}
+  </ScrollView>
 );
 
 export default RepoList;
